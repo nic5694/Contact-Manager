d09875b00c70de968f84ec9de7219127a765ff65
Nic <101201789+nic5694@users.noreply.github.com>
2022-12-07 15:35:22 -0500

    error push visual studio issues

--- ContactManager/MainWindow.xaml.cs
+++ ContactManager/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Data.SqlClient;
 using System.Configuration;
 using ContactManager.DB;
 using System.Security.Cryptography.X509Certificates;
+using ContactManager.DB.Entities;
 
 namespace ContactManager
 {
@@ -23,21 +24,19 @@ namespace ContactManager
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
+    
 
     
 
     public partial class MainWindow : Window
     {
+        string name { get; set; }
+        int age { get; set; }
+        DetailsWindow detailsWindow = new DetailsWindow();
         AddContactWindow addwindow = new AddContactWindow();
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            DataBase db = new DataBase();
-            db.GetContact(1) ;
         }
 
         private void AddContact(object sender, RoutedEventArgs e)
@@ -47,7 +46,21 @@ namespace ContactManager
 
         private void DeleteContact(object sender, RoutedEventArgs e)
         {
+            DataBase db = new DataBase();
+            Contact c = new Contact();
+            c = db.GetContact(1);
+            MessageBox.Show(c.FirstName, "Contact");
             MessageBox.Show("Are you sure you want to delete that Contact ?","Delete",MessageBoxButton.YesNo);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            /*
+            List<Contact> contacts = new List<Contact>();
+            DataBase db = new DataBase();
+            contacts = db.getAllContacts();
+            lvDataBinding.ItemsSource = contacts;*/
         }
     }
 }
